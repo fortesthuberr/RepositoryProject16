8e4a7a1bb2422388367251662129d722fbd4afe8
Your Name <you@example.com>
2026-04-26 12:59:08 +0300

    kkoololo

--- product.js
+++ product.js
@@ -47,31 +47,8 @@ let a = [
     },
 ]
 
-// `<div class="card">
-//         <div class="cir">
-//             <img src="https://avatars.mds.yandex.net/get-mpic/4012682/2a0000018e650f1c078c112fa3ec3993230c/orig" alt="" class="imageeg">
-//             <h1>Стол их клёна</h1>
-//             <p>красивый стол из клёна</p>
-//             <p class="cost">24000</p>
-//             <button>купить</button>
-//         </div>
-//     </div>`
 
-let market = document.getElementById('market')
+let parametr = location.search.replace('?parametr=' '')
 
-for(let i= 0; i < a.length; i++){
-    let object =a[i]
-    market.innerHTML = market.innerHTML + `
-    <div class="card">
-    <a href="/product.html?parametr=${object.name}">
-    <div class="cir">
-            <img src="${object.image}" alt="" class="imageeg">
-            <h1>${object.name}</h1>
-            <p>${object.material}</p>
-            <p class="cost">${object.price}</p>
-            </a>
-            <button>купить</button>
-        </div>
-    </div>
-    `
-}
+let name = document.getElementById('name')
+name.innerHTML = parametr
